9d405f049b38bde72123cbd13d702a88edee6b08
rawanazazi12 <rawanazazi12@gmail.com>
2021-06-13 18:21:13 +0300

    creating new html for stories

--- JS/home.js
+++ JS/home.js
@@ -1,32 +1,30 @@
-
-
 'use strict'
-let storiesDiv=document.getElementById('storiesDiv');
+// let storiesDiv=document.getElementById('storiesDiv');
 
-//getting the user name and his story from the form in stories page to the home page
+// //getting the user name and his story from the form in stories page to the home page
 
-function getUsersStories() {
-    let data = localStorage.getItem('stories');
-    let storiesData = JSON.parse(data);
-    if (storiesData !== null) {
-      Story.allStories = storiesData;
-    }
-  }
+// function getUsersStories() {
+//     let data = localStorage.getItem('stories');
+//     let storiesData = JSON.parse(data);
+//     if (storiesData !== null) {
+//       Story.allStories = storiesData;
+//     }
+//   }
 
-  //showing the users' data on the home page after submitting the form
+//   //showing the users' data on the home page after submitting the form
   
-  function render() {
-    for (let i = 0; i < Story.allStories.length; i++) {
-      let h3 = document.createElement('h3');
-      storiesDiv.appendChild(h3);
-      h3.textContent = Story.allStories[i].userName;
-      let paragraph = document.createElement('p');
-      storiesDiv.appendChild(paragraph);
-      paragraph.textContent = Story.allStories[i].storyText;
+//   function render() {
+//     for (let i = 0; i < Story.allStories.length; i++) {
+//       let h3 = document.createElement('h3');
+//       storiesDiv.appendChild(h3);
+//       h3.textContent = Story.allStories[i].userName;
+//       let paragraph = document.createElement('p');
+//       storiesDiv.appendChild(paragraph);
+//       paragraph.textContent = Story.allStories[i].storyText;
   
-    }
-  }
+//     }
+//   }
 
-getUsersStories();
-render();
+// getUsersStories();
+// render();
 
--- JS/stories.js
+++ JS/stories.js
@@ -2,18 +2,20 @@
 let storiesForm = document.getElementById('stories');
 let userName = document.getElementById('userName');
 let storyText = document.getElementById('storyText');
-let programName=document.getElementById('programName');
+let programs=document.getElementById('programs');
 let button = document.getElementById('submit');
 let resultsDiv = document.getElementById('results');
+let picture=document.getElementById('picture');
 
 Story.allStories = [];
 
 //creating a constructor function for user's data
 
-function Story(userName,programName, storyText) {
+function Story(userName,programs, storyText,picture) {
   this.userName = userName;
-  this.programName=programName;
+  this.programs=programs;
   this.storyText = storyText;
+  this.picture= picture;
   Story.allStories.push(this);
 }
 
@@ -23,46 +25,24 @@ storiesForm.addEventListener('submit', submitting);
 function submitting(event) {
   event.preventDefault();
   let name = event.target.userName.value;
-  let programName=event.target.programName.value;
+  let programs=event.target.programs.value;
   let story = event.target.storyText.value;
-
+  let picture=event.target.picture.value;
+  console.log(event);
   
-new Story(name, story)
-
-  
-  userStoriesArr.push(story);
-  console.log(Story.allStories);
-
-  userNamesArr.push(name);
-  userNamesArr.push(story);
-
-
-  updateStorage();
-  
-
-  
-  // Story.allStories.push(userNamesArr);
-  // Story.allStories.push(userStoriesArr);
-  render();
-
-
-  // console.log(allStories[0]+'hi');
-  // console.log(userNamesArr);
-  // console.log(userStoriesArr);
-
   //creating a new story to save the user's input data
-  new Story(name,programName,story)
+  new Story(name,programs,story,picture)
   updateStorage();
   resultsDiv.textContent='';
-  // render();
+  render();
   storiesForm.removeEventListener('submit', submitting);
-  window.location.href= "index.html";
+  // window.location.href= "index.html";
 
 }
 
 //creating a new story to be shown as a first story
 
-new Story("Toleen", "Hello there, hope you are doing well, I was a hiker in Ibn-Hammad trip, it was amazing trip with the best team ever, I will recommend this trip to my friends^^");
+new Story("Toleen",' ' ,"Hello there, hope you are doing well, I was a hiker in Ibn-Hammad trip, it was amazing trip with the best team ever, I will recommend this trip to my friends^^");
 
 //storing the data in the local storage
 
@@ -70,35 +50,6 @@ function updateStorage() {
   let storageArr = JSON.stringify(Story.allStories);
   localStorage.setItem('stories', storageArr);
   
-
-  // let nameArr= JSON.stringify(Story.allStories);
-  // localStorage.setItem('userName', nameArr);
-
-  
-  
-
-}
-
-
-let resultsDiv = document.getElementById('results');
-function render() {
-  console.log(Story.allStories);
-  for (let i = 0; i <Story.allStories.length; i++) {
-
-    let h3 = document.createElement('h3');
-    resultsDiv.appendChild(h3);
-    h3.textContent = Story.allStories[i].userName;
-    let paragraph = document.createElement('p');
-    resultsDiv.appendChild(paragraph);
-    paragraph.textContent = Story.allStories[i].storyText;
-    // userNamesArr=[];
-    // userStoriesArr=[];
-
-
-  }
-
-
-
 }
 
 //getting the user's data from the local storage
@@ -110,24 +61,25 @@ function getUsersStories() {
   }
 }
 
-getUsersStories();
 
-render()
+function render() {
+  for (let i = 0; i < Story.allStories.length; i++) {
+    let h3 = document.createElement('h3');
+    resultsDiv.appendChild(h3);
+    h3.textContent = Story.allStories[i].userName;
+    let paragraph = document.createElement('p');
+    resultsDiv.appendChild(paragraph);
+    paragraph.textContent = Story.allStories[i].storyText;
+    let img=document.createElement('a');
+    resultsDiv.appendChild(img);
+    img.textContent=Story.allStories[i].picture;
 
+    console.log(img);
 
-// function render() {
-//   for (let i = 0; i < Story.allStories.length; i++) {
-//     let h3 = document.createElement('h3');
-//     resultsDiv.appendChild(h3);
-//     h3.textContent = Story.allStories[i].userName;
-//     let paragraph = document.createElement('p');
-//     resultsDiv.appendChild(paragraph);
-//     paragraph.textContent = Story.allStories[i].storyText;
-
-//   }
+  }
  
-// }
+}
 
 
 getUsersStories();
-// render();
+render();
