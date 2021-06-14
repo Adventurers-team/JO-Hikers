4bda1cf2364e8f9787ae8422d36cd62f1e6cd2a8
sarah-hillo <alhillosarah@gmail.com>
2021-06-14 05:38:28 +0300

    changes

--- JS/catalogue.js
+++ JS/catalogue.js
@@ -51,12 +51,6 @@ let totalPrice =0;
 
 // let checkedbox=[];
 
-
-
-
-// When someone submits the form, we need to add the selected item to the cart
-// object, save the whole thing back to local storage and update the screen
-// so that it shows the # of items in the cart and a quick preview of the cart itself.
 let newquant=[];
 let newPrice1=1;
 function handleSubmit(event) {
